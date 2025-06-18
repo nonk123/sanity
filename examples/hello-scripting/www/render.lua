35e38d75f39c4b110d1b4b9052ac380892edb887
nonk123 <me@nonk.dev>
2025-06-18 17:31:58 +0300

    Nitpick on Lua

--- examples/hello-scripting/www/render.lua
+++ examples/hello-scripting/www/render.lua
@@ -3,11 +3,11 @@
 -- In this example, we're rendering a personalized wedding invitation for each of our guests.
 
 local guests = { "Bob", "Fred", "Mars", "Luca", "Miguel" };
-render("_index.html", "index.html", { ["default"] = guests[1] });
+render("_index.html", "index.html", { default = guests[1] });
 
 for _, name in pairs(guests) do
     render("_page.html", name .. ".html", {
-        ["name"] = name,
-        ["guests"] = guests,
+        name = name,
+        guests = guests,
     });
 end
